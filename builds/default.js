dcbc47637cca8589c3508b7ca428e55eafd3599a
Dejan Kamenšek <kamensek@gmail.com>
2023-02-28 20:05:48 +0100

    Update default.js

--- builds/default.js
+++ builds/default.js
@@ -1,8 +1,8 @@
 module.exports = {
     board_env: "STM32F103RE_creality",
     meta: {
-        stable_name: "ender_3_4.2.2-{{marlin_version}}-default-{{uid}}",
-        nightly_name: "ender_3_4.2.2-{{current_date}}-default-{{uid}}"
+        stable_name: "ender_3pro_v4.2.2-{{marlin_version}}-default-{{uid}}",
+        nightly_name: "ender_3pro_v4.2.2-{{current_date}}-default-{{uid}}"
     },
     based_on: {
         repo: "https://github.com/MarlinFirmware/Configurations/",
