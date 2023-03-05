7cee8d92779e56730d0d5459833cf22a388fce85
Dejan Kamenšek <kamensek@gmail.com>
2023-03-05 22:54:51 +0100

    Update default.js

--- builds/default.js
+++ builds/default.js
@@ -1,4 +1,6 @@
 module.exports = {
+
+
     board_env: "STM32F103RE_creality",
     active: true,
     //only: "stable",   // build only stable branch
@@ -7,24 +9,36 @@ module.exports = {
         nightly_name: "ender3pro_v4.2.2_{{current_date}}-default-{{uid}}"
     },
 
-  based_on: {   // default Ender-3 Pro Marlin example config
+
+    based_on: {   // default Ender-3 Pro Marlin example config
         repo: "https://github.com/MarlinFirmware/Configurations/",
         path: "/config/examples/Creality/Ender-3 Pro/CrealityV422/",
         stable_branch: "release-2.1.2",   // latest stable
         nightly_branch: "bugfix-2.1.x"    // latest nightly
     },
-        configuration: {
+    
+    
+    configuration:     {   // "Configuration.h" customizations
         enable: [
+
             ["STRING_CONFIG_H_AUTHOR", "(Dust, valerionew, Ender-3, zisismaras, KamensekD)"],
+
             //standard leveling menu helper
             "LCD_BED_TRAMMING",
             "BED_TRAMMING_INCLUDE_CENTER"
+
         ]
     },
-    configuration_adv: {
+    
+    
+    configuration_adv: {   // "Configuration_adv.h" customizations
         enable: [
+
             //octoprint
             "HOST_ACTION_COMMANDS"
+
         ]
     }
+
+
 };
