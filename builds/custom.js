b52d6eb275b10a2115815c00575987b79c07f43b
Dejan Kamenšek <kamensek@gmail.com>
2023-03-06 01:02:26 +0100

    Update custom.js

--- builds/custom.js
+++ builds/custom.js
@@ -24,12 +24,32 @@ module.exports = {
     configuration:     {   // "Configuration.h" customizations
         enable: [
 
+            
+            
             ["STRING_CONFIG_H_AUTHOR", "(Dust, valerionew, Ender-3, zisismaras, KamensekD)"],
 
-            //standard leveling menu helper
+            
+            //Standard leveling menu helper
             "LCD_BED_TRAMMING",
             "BED_TRAMMING_INCLUDE_CENTER",
 
+            
+            //Stepper Driver Types
+            //"Creality 4.2.2 boards come with a variety of stepper drivers.
+            //Check the board label (typically on SD Card module) and set the correct *_DRIVER_TYPE!
+            //(C=HR4988, E=A4988, A=TMC2208, B=TMC2209, H=TMC2225, H8=HR4988)
+            //* Use TMC2208/TMC2208_STANDALONE for TMC2225 drivers and TMC2209/TMC2209_STANDALONE for TMC2226 drivers.
+            //* Options: A4988, A5984, DRV8825, LV8729, TB6560, TB6600, TMC2100,
+            //*          TMC2130, TMC2130_STANDALONE, TMC2160, TMC2160_STANDALONE,
+            //*          TMC2208, TMC2208_STANDALONE, TMC2209, TMC2209_STANDALONE,
+            //*          TMC26X,  TMC26X_STANDALONE,  TMC2660, TMC2660_STANDALONE,
+            //*          TMC5130, TMC5130_STANDALONE, TMC5160, TMC5160_STANDALONE
+            ["X_DRIVER_TYPE", "A4988"],
+            ["Y_DRIVER_TYPE", "A4988"],
+            ["Z_DRIVER_TYPE", "A4988"],
+            ["E0_DRIVER_TYPE", "A4988"],
+            
+            
             //Preheat Constants
             ["PREHEAT_1_LABEL", "PLA"],
             ["PREHEAT_1_TEMP_HOTEND", 200],
@@ -39,6 +59,7 @@ module.exports = {
             ["PREHEAT_2_TEMP_HOTEND", 225],
             ["PREHEAT_2_TEMP_BED", 50],
  
+            
         ]
     },
 
@@ -47,9 +68,13 @@ module.exports = {
     configuration_adv: {   // "Configuration_adv.h" customizations
         enable: [
 
+            
+            
             //octoprint
             "HOST_ACTION_COMMANDS"
 
+            
+            
         ]
     }
 
