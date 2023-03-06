8ef35929c0c503977353e03be5cdfffa562f3474
Dejan Kamenšek <kamensek@gmail.com>
2023-03-06 12:08:09 +0100

    Update custom.js

--- builds/custom.js
+++ builds/custom.js
@@ -20,7 +20,8 @@ module.exports = {
     },
 
     
-
+                           // refer to:   https://github.com/zisismaras/marlin_auto_build
+                           // for valid syntax
     configuration:     {   // "Configuration.h" customizations
         enable: [
 
@@ -44,10 +45,10 @@ module.exports = {
             //*          TMC2208, TMC2208_STANDALONE, TMC2209, TMC2209_STANDALONE,
             //*          TMC26X,  TMC26X_STANDALONE,  TMC2660, TMC2660_STANDALONE,
             //*          TMC5130, TMC5130_STANDALONE, TMC5160, TMC5160_STANDALONE
-            ["X_DRIVER_TYPE", 'A4988'],
-            ["Y_DRIVER_TYPE", 'A4988'],
-            ["Z_DRIVER_TYPE", 'A4988'],
-            ["E0_DRIVER_TYPE", 'A4988'],
+            ["X_DRIVER_TYPE",  q`A4988`],
+            ["Y_DRIVER_TYPE",  q`A4988`],
+            ["Z_DRIVER_TYPE",  q`A4988`],
+            ["E0_DRIVER_TYPE", q`A4988`],
             
             
             //Preheat Constants
