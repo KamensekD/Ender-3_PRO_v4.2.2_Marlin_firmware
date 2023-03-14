feea4252dbaa1d87fb50f0ca8c11c9c8e4b1ac40
Dejan Kamenšek <kamensek@gmail.com>
2023-03-14 14:38:08 +0100

    Update base.js

--- builds/base.js
+++ builds/base.js
@@ -3,7 +3,7 @@ module.exports = {
     board_env: "STM32F103RE_creality",
     //active: false,    // disable this build
     //only: "stable",   // build only stable or nightly branch
-    only: "nightly",   // build only stable or nightly branch
+      //only: "nightly",   // build only stable or nightly branch
     meta: {
         stable_name: "ender3pro_v4.2.2_M{{marlin_version}}-base-{{current_date}}",
         nightly_name: "ender3pro_v4.2.2_{{current_date}}-base-{{uid}}"
