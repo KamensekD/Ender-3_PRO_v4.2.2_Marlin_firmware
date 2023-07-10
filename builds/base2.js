8468e6e48c23fd083cdfe7cad7d2e86823f1c94e
Dejan Kamenšek <kamensek@gmail.com>
2023-07-10 14:18:35 +0200

    Update and rename base_import.js to base2.js

--- builds/base2.js
+++ builds/base2.js
@@ -5,16 +5,16 @@ module.exports = {
     only: "stable",   // build only stable or nightly branch
       //only: "nightly",   // build only stable or nightly branch
     meta: {
-        stable_name: "ender3pro_v4.2.2_R2121M_{{marlin_version}}-base-{{current_date}}",
-        nightly_name: "ender3pro_v4.2.2_import_{{current_date}}-base-{{uid}}"
+        stable_name: "ender3pro_v4.2.2_M{{marlin_version}}-base2-{{current_date}}",
+        nightly_name: "ender3pro_v4.2.2_import_{{current_date}}-base2-{{uid}}"
     },
 
     based_on: {   // default Ender-3 Pro Marlin example config
         repo: "https://github.com/MarlinFirmware/Configurations/",
         path: "/config/examples/Creality/Ender-3 Pro/CrealityV422/",
-   //     stable_branch: "release-{{marlin_version}}",   // latest stable
+        stable_branch: "release-{{marlin_version}}",   // latest stable
         stable_branch: "release-2.1.2.1",              // latest stable release
-   //     nightly_branch: "import-2.1.x"                 // latest import
+        nightly_branch: "import-2.1.x"                 // latest import
     },
 
 
