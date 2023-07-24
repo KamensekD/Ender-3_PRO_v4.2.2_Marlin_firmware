d7220bc8fd0858ca35569c473ad320abe27bbff6
Dejan Kamenšek <kamensek@gmail.com>
2023-07-24 12:18:55 +0200

    Update base.js

--- builds/base.js
+++ builds/base.js
@@ -2,8 +2,8 @@ module.exports = {
 
     board_env: "STM32F103RE_creality",
     //active: false,    // disable this build
-    only: "stable",   // build only stable or nightly branch
-    //only: "nightly",   // build only stable or nightly branch
+    //only: "stable",   // build only stable or nightly branch
+    only: "nightly",   // build only stable or nightly branch
     meta: {
         stable_name: "ender3pro_v4.2.2_M{{marlin_version}}-base-{{current_date}}",
         nightly_name: "ender3pro_v4.2.2_{{current_date}}-base-{{uid}}"
@@ -26,7 +26,8 @@ module.exports = {
         repo: "https://github.com/MarlinFirmware/Configurations/",
         path: "/config/examples/Creality/Ender-3 Pro/CrealityV422/",
 //        stable_branch: "import-2.1.x",              // latest stable
-        stable_branch: "release-2.0.9.7",              // latest stable
+//        stable_branch: "release-2.0.9.7",              // latest stable
+        stable_branch: "release-{{marlin_version}}",   // latest stable
         nightly_branch: "bugfix-2.1.x"                 // latest nightly
     },
     
