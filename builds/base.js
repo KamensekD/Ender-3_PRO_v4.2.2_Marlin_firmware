c5ef32f16e4a03608e92409b2c4daeb533bd2872
Dejan Kamenšek <kamensek@gmail.com>
2023-07-31 13:13:05 +0200

    Update base.js

--- builds/base.js
+++ builds/base.js
@@ -5,10 +5,11 @@ module.exports = {
     //only: "stable",   // build only stable or nightly branch
     //only: "nightly",   // build only stable or nightly branch
     meta: {
+        nightly_name: "ender3pro_v4.2.2_M2.1.x-bugfix-base-{{current_date}}"
         stable_name: "ender3pro_v4.2.2_M{{marlin_version}}-base-{{current_date}}",
 //        stable_name: "ender3pro_v4.2.2_M2.0.9.7-base-{{current_date}}",
 //        nightly_name: "ender3pro_v4.2.2_{{current_date}}-base-{{uid}}"
-        nightly_name: "ender3pro_v4.2.2_{{marlin_version}}-base-{{current_date}}"
+//        nightly_name: "ender3pro_v4.2.2_{{marlin_version}}-base-{{current_date}}"
     },
 
 // Creating builds based on other's configurations:
