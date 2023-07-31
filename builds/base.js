24ad2dfbf1941c4144894bf2e9c570d5bf460f82
Dejan Kamenšek <kamensek@gmail.com>
2023-07-31 11:47:55 +0200

    Update base.js

--- builds/base.js
+++ builds/base.js
@@ -3,7 +3,7 @@ module.exports = {
     board_env: "STM32F103RE_creality",
     //active: false,    // disable this build
     //only: "stable",   // build only stable or nightly branch
-    only: "nightly",   // build only stable or nightly branch
+    //only: "nightly",   // build only stable or nightly branch
     meta: {
         stable_name: "ender3pro_v4.2.2_M{{marlin_version}}-base-{{current_date}}",
         nightly_name: "ender3pro_v4.2.2_{{current_date}}-base-{{uid}}"
@@ -22,23 +22,23 @@ module.exports = {
 // Instead of using latest you can point to the currently building marlin version by using {{marlin_version}}
 // or hard-code a specific version that you want. Using latest is recommended.
 
-    based_on: {   // default Ender-3 Pro Marlin example config
-        repo: "https://github.com/MarlinFirmware/Configurations/",
-        path: "/config/examples/Creality/Ender-3 Pro/CrealityV422/",
+//    based_on: {   // default Ender-3 Pro Marlin example config
+//        repo: "https://github.com/MarlinFirmware/Configurations/",
+//        path: "/config/examples/Creality/Ender-3 Pro/CrealityV422/",
 //        stable_branch: "import-2.1.x",              // latest stable
 //        stable_branch: "release-2.0.9.7",              // latest stable
-        stable_branch: "release-{{marlin_version}}",   // latest stable
-        nightly_branch: "bugfix-2.1.x"                 // latest nightly
+//        stable_branch: "release-{{marlin_version}}",   // latest stable
+//        nightly_branch: "bugfix-2.1.x"                 // latest nightly
     },
     
 
     
-//    based_on: {   // default Ender-3 Pro Marlin example config
-//        repo: "https://github.com/MarlinFirmware/Configurations/",
-//        path: "/config/examples/Creality/Ender-3 Pro/CrealityV422/",
-//        stable_branch: "release-{{marlin_version}}",   // latest stable
-//        nightly_branch: "bugfix-2.1.x"                 // latest nightly
-//    },
+    based_on: {   // default Ender-3 Pro Marlin example config
+        repo: "https://github.com/MarlinFirmware/Configurations/",
+        path: "/config/examples/Creality/Ender-3 Pro/CrealityV422/",
+        stable_branch: "release-{{marlin_version}}",   // latest stable
+        nightly_branch: "bugfix-2.1.x"                 // latest nightly
+    },
 
 
     
