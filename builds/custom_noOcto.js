4480b2a34127fe08b2dff867b22971ad61df5d79
Dejan Kamenšek <kamensek@gmail.com>
2023-08-01 07:38:06 +0200

    Update custom_noOcto.js

--- builds/custom_noOcto.js
+++ builds/custom_noOcto.js
@@ -1,16 +1,14 @@
 module.exports = {
     extends: "builds/custom.js",
-//    active: false,    // disable this build
+    active: false,    // disable this build
 //    only: "stable",   // build only stable or nightly branch
 //    only: "nightly",   // build only stable or nightly branch
 
 
     meta: {
         stable_name: "ender3pro_v4.2.2_M{{marlin_version}}-customNoOcto-{{current_date}}",
-        nightly_name: "ender3pro_v4.2.2_M2.1.x-bugfix-customNoOcto-{{current_date}}"
+        nightly_name: "ender3pro_v4.2.2_M2.1.x-bugfix{{marlin_version}}-customNoOcto-{{current_date}}"
     },
-//        stable_name: "ender3pro_v4.2.2_M{{marlin_version}}-customNO-{{current_date}}",
-//        nightly_name: "ender3pro_v4.2.2_{{current_date}}-customNO-{{uid}}"
 
     
 // for valid syntax refer to: https://github.com/zisismaras/marlin_auto_build
