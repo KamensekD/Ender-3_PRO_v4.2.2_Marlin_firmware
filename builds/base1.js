0447f87fa2a31a649e37fc16b0bec93003d2eb98
Dejan Kamenšek <kamensek@gmail.com>
2023-08-04 11:35:35 +0200

    Create base1.js

---
+++ builds/base1.js
@@ -0,0 +1,23 @@
+module.exports = {
+    //active: false,    // disable this build
+    //only: "stable",   // build only stable or nightly branch
+    //only: "nightly",   // build only stable or nightly branch
+    board_env: "STM32F103RE_creality",
+    min_version: "2.1.2",
+    
+    meta: {
+        stable_name: "ender3pro_v4.2.2_{{current_date}}_M{{marlin_version}}-base1",
+        nightly_name: "ender3pro_v4.2.2_{{current_date}}_M2.1.x_bugfix{{marlin_version}}-base1"
+//        nightly_name: "ender3pro_v4.2.2_{{marlin_version}}-base1-{{current_date}}"
+//        nightly_name: "ender3pro_v4.2.2_{{current_date}}-base1-{{uid}}"
+    },
+
+    based_on: {     // default Ender-3 Pro Marlin example config
+        repo: "https://github.com/MarlinFirmware/Configurations/",
+        path: "/config/examples/Creality/Ender-3 Pro/CrealityV422/",
+        stable_branch: "release-{{marlin_version}}",
+        nightly_branch: "bugfix-2.1.x"
+//        stable_branch: "release-2.0.9.7",              // doesn't seem to work
+//        stable_branch: "release-2.1.2.1",              // doesn't seem to work
+    },
+};
