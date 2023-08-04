0c77b4162929b87af390127a499cb1eb90c2f3c8
Dejan Kamenšek <kamensek@gmail.com>
2023-08-04 13:29:29 +0200

    Update base.js

--- builds/base.js
+++ builds/base.js
@@ -2,22 +2,29 @@ module.exports = {
     //active: false,    // disable this build
     //only: "stable",   // build only stable or nightly branch
     //only: "nightly",   // build only stable or nightly branch
-    board_env: "STM32F103RE_creality",
-    min_version: "2.1.2",
-    
+
+    extends: "builds/default.js",    
+
     meta: {
         stable_name: "ender3pro_v4.2.2_{{current_date}}_M{{marlin_version}}-base",
         nightly_name: "ender3pro_v4.2.2_{{current_date}}_M2.1.x_bugfix{{marlin_version}}-base"
-//        nightly_name: "ender3pro_v4.2.2_{{marlin_version}}-base-{{current_date}}"
-//        nightly_name: "ender3pro_v4.2.2_{{current_date}}-base-{{uid}}"
     },
 
-    based_on: {     // default Ender-3 Pro Marlin example config
-        repo: "https://github.com/MarlinFirmware/Configurations/",
-        path: "/config/examples/Creality/Ender-3 Pro/CrealityV422/",
-        stable_branch: "release-{{marlin_version}}",
-        nightly_branch: "bugfix-2.1.x"
-//        stable_branch: "release-2.0.9.7",              // doesn't seem to work
-//        stable_branch: "release-2.1.2.1",              // doesn't seem to work
+    configuration: {
+        enable: [
+            ["STRING_CONFIG_H_AUTHOR", "(KamensekD)"],
+            //standard leveling menu helper
+            "LCD_BED_TRAMMING",
+            "BED_TRAMMING_INCLUDE_CENTER"
+        ]
     },
+    configuration_adv: {
+        enable: [
+            //octoprint
+            "HOST_ACTION_COMMANDS"
+        ]
+    }
+
+
+    
 };
