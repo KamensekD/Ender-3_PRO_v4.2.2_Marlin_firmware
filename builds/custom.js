d80b787b9b7318448cf19f02aee3e618e350fe2b
Dejan Kamenšek <kamensek@gmail.com>
2023-08-04 11:16:50 +0200

    Update custom.js

--- builds/custom.js
+++ builds/custom.js
@@ -8,50 +8,5 @@ module.exports = {
         stable_name: "ender3pro_v4.2.2_{{current_date}}_M{{marlin_version}}-custom",
         nightly_name: "ender3pro_v4.2.2_{{current_date}}_M2.1.x_bugfix{{marlin_version}}-custom"
     },
-   
-
-// for valid syntax refer to: https://github.com/zisismaras/marlin_auto_build
-    configuration:     {   // "Configuration.h" customizations
-        enable: [
-
-
-            ["STRING_CONFIG_H_AUTHOR", "(KamensekD)"],
-            ["CUSTOM_MACHINE_NAME", "Ender-3 Pro v4.2.2"],
-
-        ],
-        disable: [
-
-
-            "Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN",
-            "MULTIPLE_PROBING",
-            "EXTRA_PROBING",
-
-        ]
-    },
-
-
-    configuration_adv: {   // "Configuration_adv.h" customizations
-        enable: [
-
-
-            //for Octoprint
-            "HOST_ACTION_COMMANDS",
-
-            
-            "HOST_STATUS_NOTIFICATIONS",
-
-            
-
-
-        ],
-        disable: [
-
-
-            "POWER_LOSS_RECOVERY",   //disable this to save SD card writes for each layer
-            "SDCARD_READONLY",       //read/write access to SD card
-
-
-        ]
-    }
 
 };
